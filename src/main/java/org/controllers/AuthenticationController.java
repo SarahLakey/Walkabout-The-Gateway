95b7a13ba89e5ebd08a06db984359f75be642f11
SarahLakey <salbers1212@gmail.com>
2024-01-21 16:19:41 -0600

    updated AuthenticationController get GET and POST handlers. Added basic templates for login.html and register.html

--- src/main/java/org/controllers/AuthenticationController.java
+++ src/main/java/org/controllers/AuthenticationController.java
@@ -1,10 +1,19 @@
 package org.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import org.data.UserRepository;
 import org.models.User;
+import org.models.dto.LoginFormDTO;
+import org.models.dto.RegistrationFormDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Optional;
 
@@ -16,7 +25,7 @@ public class AuthenticationController {
 
     private static final String userSessionKey = "user";
 
-    private static void setUserSessionKey(HttpSession session, User user) {
+    private static void setUserInSession(HttpSession session, User user) {
         session.setAttribute(userSessionKey, user.getId());
     }
 
@@ -35,4 +44,74 @@ public class AuthenticationController {
         return userOpt.get();
 
     }
+
+
+    @GetMapping("/register")
+    public String displayRegistrationForm(Model model, HttpSession session) {
+        model.addAttribute(new RegistrationFormDTO());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String processRegistrationForm(@ModelAttribute @Valid RegistrationFormDTO registrationFormDTO,
+                                          Errors errors, HttpServletRequest request) {
+        //send user back to form if errors are found
+        if (errors.hasErrors()) {
+            return "register";
+        }
+
+        User existingUser = userRepository.findByUsername(registrationFormDTO.getUsername());
+
+        if (existingUser != null) {
+            errors.rejectValue("username", "username.alreadyExists",
+                    "A user with that username already exists");
+            return "register";
+        }
+
+        String password = registrationFormDTO.getPassword();
+        String verifyPassword = registrationFormDTO.getVerifyPassword();
+        if (!password.equals(verifyPassword)) {
+            errors.rejectValue("password", "passwords.mismatch",
+                    "Passwords do not match");
+            return "register";
+        }
+
+        User newUser = new User(registrationFormDTO.getUsername(), registrationFormDTO.getPassword());
+        userRepository.save(newUser);
+        setUserInSession(request.getSession(), newUser);
+        return "redirect:/index";
+    }
+
+    @GetMapping("/login")
+    public String displayLoginForm(Model model, HttpSession session) {
+        model.addAttribute(new LoginFormDTO());
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String processLoginForm(@ModelAttribute @Valid LoginFormDTO loginFormDTO, Errors errors,
+                                   HttpServletRequest request){
+        if(errors.hasErrors()){
+            return "login";
+        }
+
+        User theUser = userRepository.findByUsername(loginFormDTO.getUsername());
+
+        String password = loginFormDTO.getPassword();
+
+        if (theUser == null || !theUser.isMatchingPassword(password)) {
+            errors.rejectValue("password", "login.invalid",
+                    "Credentials invalid. Please try again with correct username/password.");
+            return "login";
+        }
+        setUserInSession(request.getSession(), theUser);
+        return "redirect:/index";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        request.getSession().invalidate();
+        return "redirect:/login";
+    }
+
 }
