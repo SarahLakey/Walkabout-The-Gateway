783e35057e97b306d3b203d37e4c3127e343ce12
SarahLakey <salbers1212@gmail.com>
2024-01-21 13:52:34 -0600

    Updated User class and UserRepository

--- src/main/java/org/models/User.java
+++ src/main/java/org/models/User.java
@@ -2,9 +2,11 @@ package org.models;
 
 import jakarta.persistence.Entity;
 import jakarta.validation.constraints.NotNull;
+import org.models.dto.AbstractEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Entity
-public class User {
+public class User extends AbstractEntity {
 
     @NotNull
     private String username;
@@ -14,6 +16,19 @@ public class User {
 
     public User() {}
 
+    public User(String username, String password) {
+        this.username = username;
+        this.pwHash = encoder.encode(password);
+    }
 
+    public String getUsername() {
+        return username;
+    }
+
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    public boolean isMatchingPassword (String password) {
+        return encoder.matches(password, pwHash);
+    }
 
 }
