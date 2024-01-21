9b2b60edd83261b5dbdb822975e607301847c39c
JillianJaclyn <jillian.fussner@gmail.com>
2024-01-21 16:10:12 -0600

    Added UserModel class for user creation and started linking MySQL

--- src/main/java/authentication/models/dto/controllers/models/UserModel.java
+++ src/main/java/authentication/models/dto/controllers/models/UserModel.java
@@ -1,4 +1,33 @@
 package authentication.models.dto.controllers.models;
 
-public class UserModel {
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Entity
+public class UserModel extends AbstractEntity {
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String pwHash;
+
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    public UserModel(String username, String password) {
+        this.username = username;
+        this.pwHash = encoder.encode(password);
+    }
+
+    public UserModel() {}
+
+    public String getUsername() {
+        return username;
+    }
+
+    public boolean isMatchingPassword(String password) {
+        return encoder.matches(password, pwHash);
+    }
+
 }
