b83e2ab9e02f1c7f756123765d57c19eafd2c5ac
jojotier <122423763+jojotier@users.noreply.github.com>
2024-01-20 20:00:26 -0600

    created page and controller to create a page to display a map once googlemaps api is properly imported

--- src/main/java/controllers/DisplayMapPageController.java
+++ src/main/java/controllers/DisplayMapPageController.java
@@ -1,8 +1,14 @@
 package controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DisplayMapPageController {
+
+    @GetMapping("display-map")
+    public String displayMapController(){
+        return "map-display";
+    }
 
 }
