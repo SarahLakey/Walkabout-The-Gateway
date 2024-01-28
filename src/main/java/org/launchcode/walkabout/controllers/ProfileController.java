06680dfa59ee09ff1d71486df18f96655cddf41e
SarahLakey <salbers1212@gmail.com>
2024-01-28 12:11:21 -0600

    updated css

--- src/main/java/org/launchcode/walkabout/controllers/ProfileController.java
+++ src/main/java/org/launchcode/walkabout/controllers/ProfileController.java
@@ -1,13 +1,21 @@
 package org.launchcode.walkabout.controllers;
 
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProfileController {
 
-    @GetMapping("profile")
-        public String displayUserProfile() {
+    //TODO: Add @Autowired annotation + repositories
+
+        @GetMapping("profile")
+        public String displayUserProfile(Model model, HttpSession session) {
+            model.addAttribute("loggedIn", session.getAttribute("user") != null);
+
             return "profile";
         }
+
     }
