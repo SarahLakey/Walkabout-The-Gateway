3873b75ae25d16173937e721e3903f58c596c655
SarahLakey <salbers1212@gmail.com>
2024-01-28 12:16:02 -0600

    commented out button controller and dto to fix merge conflict

--- src/main/java/org/launchcode/walkabout/models/dto/ReportButtonDTO.java
+++ src/main/java/org/launchcode/walkabout/models/dto/ReportButtonDTO.java
@@ -1,19 +1,17 @@
-//package src.main.java.org.launchcode.walkabout.models.dto;
-//
-//import org.jetbrains.annotations.NotNull;
-//
-//public class ReportButtonDTO {
-//
-//    @NotNull
-//    private Event event;
-//
-//    public ReportButtonDTO(){};
-//
-//    public Event getEvent() {
-//        return event;
-//    }
-//
-//    public void setEvent(Event event) {
-//        this.event = event;
-//    }
-//}
+package src.main.java.org.launchcode.walkabout.models.dto;
+
+/* public class ReportButtonDTO {
+
+    @NotNull
+    private Event event;
+
+    public ReportButtonDTO(){};
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+} */
