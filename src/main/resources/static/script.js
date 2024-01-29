472d54dc746351d260294642497b11e93864731b
jojotier <122423763+jojotier@users.noreply.github.com>
2024-01-29 11:51:45 -0600

    refactored map-display template and updated script.js file. coordinates also updated: map should display st louis map

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,1 +1,22 @@
 let appName = "Walkabout the Gateway";
+
+        google.charts.load("current", {
+          "packages":["map"],
+
+          "mapsApiKey": "AIzaSyD4EMSLU3zejs7I5l--s6YTyNuzbz2Anz4"
+        });
+         google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+        let data = google.visualization.arrayToDataTable([
+          ['Lat', 'Long', 'Name'],
+          [38.6270,  -90.1994, 'St. Louis'],
+          [38.65214508187943, -90.2595139, 'LaunchCode'],
+
+        ]);
+
+        let map = new google.visualization.Map(document.getElementById('map_div'));
+        map.draw(data, {
+          showTooltip: true,
+          showInfoWindow: true
+        });
+      }
