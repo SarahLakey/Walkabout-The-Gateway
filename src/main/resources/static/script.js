65f1761b42d885cbea41a6de8479b96440cbe0cc
SarahLakey <salbers1212@gmail.com>
2024-02-02 18:10:45 -0600

    added submit facts form and updated css

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,1 +1,9 @@
 let appName = "Walkabout the Gateway";
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
