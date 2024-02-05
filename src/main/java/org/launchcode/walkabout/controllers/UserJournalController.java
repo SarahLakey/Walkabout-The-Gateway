9ba7fed2f0ee95cbb46955190e44d850a0b05dc0
sammcmurtrey <mcmurtreysamantha@gmail.com>
2024-02-05 15:41:26 -0600

    added readjournals html and user journal controller

---
+++ src/main/java/org/launchcode/walkabout/controllers/UserJournalController.java
@@ -0,0 +1,16 @@
+package org.launchcode.walkabout.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("userjournal")
+public class UserJournalController {
+    @GetMapping
+    public String displayMyJournal() {
+
+
+        return "userjournal.html";
+    }
+}
