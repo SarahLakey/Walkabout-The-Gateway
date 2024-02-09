e26bc27311303de8f2893daa184f1e33a96babe0
sammcmurtrey <mcmurtreysamantha@gmail.com>
2024-02-08 21:44:59 -0600

    working on report button in footer

---
+++ src/main/java/org/launchcode/walkabout/models/ReportButton.java
@@ -0,0 +1,31 @@
+package org.launchcode.walkabout.models;
+
+public class ReportButton {
+    private String name;
+    private String report;
+    private String otherUsername;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getReport() {
+        return report;
+    }
+
+    public void setReport(String report) {
+        this.report = report;
+    }
+
+    public String getOtherUsername() {
+        return otherUsername;
+    }
+
+    public void setOtherUsername(String otherUsername) {
+        this.otherUsername = otherUsername;
+    }
+}
