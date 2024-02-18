8024b0926c44a5430ccef5d631978d84ebb3b434
sammcmurtrey <mcmurtreysamantha@gmail.com>
2024-02-17 21:09:37 -0600

    working on community page and comments page

---
+++ src/main/java/org/launchcode/walkabout/controllers/CommentController.java
@@ -0,0 +1,51 @@
+package org.launchcode.walkabout.controllers;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import org.launchcode.walkabout.data.JournalRepository;
+import org.launchcode.walkabout.models.User;
+import org.launchcode.walkabout.models.dto.UserJournal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/comment")
+public class CommentController {
+    @Autowired
+    private JournalRepository journalRepository;
+    @Autowired
+    private AuthenticationController authenticationController;
+    private User getCurrentUser(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        return authenticationController.getUserFromSession(session);
+
+    }
+    @GetMapping
+    public String displayEditController(HttpServletRequest request, HttpSession session, Model model){
+        List<UserJournal> journalEntries = (List<UserJournal>) journalRepository.findAll();
+
+        model.addAttribute("user", new User());
+        model.addAttribute("loggedIn", session.getAttribute("user") != null);
+        model.addAttribute("userjournal", new UserJournal());
+        model.addAttribute("journalEntries", journalEntries);
+        return "journalentries/comment";
+    }
+
+    @PostMapping("/comments")
+    public String processUserJournal(@Valid UserJournal userJournal, Errors errors, Model model, HttpSession session, HttpServletRequest request) {
+        UserJournal journalEntry = new UserJournal(getCurrentUser(request).getUsername(), userJournal.getJournalEntryBlank(), userJournal.getJournalLocation(), userJournal.getJournalDate());
+        //UserJournal journalEntry = new UserJournal();
+        //journalEntry.setJournalEntryBlank(userJournal.getJournalEntryBlank() + userJournal.getJournalEntryBlank());
+        journalRepository.save(journalEntry);
+        return "redirect:/communityjournals";
+    }
+
+}
---
+++ src/main/java/org/launchcode/walkabout/controllers/CommunityJournalsController.java
@@ -0,0 +1,39 @@
+package org.launchcode.walkabout.controllers;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.launchcode.walkabout.data.JournalRepository;
+import org.launchcode.walkabout.models.User;
+import org.launchcode.walkabout.models.dto.UserJournal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+@Controller
+@RequestMapping("/communityjournals")
+public class CommunityJournalsController {
+    @Autowired
+    private JournalRepository journalRepository;
+    @Autowired
+    private UserJournalController userJournalController;
+    @Autowired
+    private AuthenticationController authenticationController;
+    @GetMapping
+    public String ReadJournalsControllerDisplay(HttpServletRequest request, HttpSession session, Model model) {
+        String journalLocation = authenticationController.getUserFromSession(session).getUsername();
+        //String journalLocation = userJournalController.displayUserJournal(journalLocation);
+        List<UserJournal> journalEntries = (List<UserJournal>) journalRepository.findAll();
+        Collections.reverse(journalEntries);
+
+        model.addAttribute("user", new User());
+        model.addAttribute("loggedIn", session.getAttribute("user") != null);
+        model.addAttribute("userjournal", new UserJournal());
+        model.addAttribute("journalEntries", journalEntries);
+        return "journalentries/communityjournals";}
+}
