cd1465a03713873b4c46bc901942e8cff88dc18f
jojotier <122423763+jojotier@users.noreply.github.com>
2024-02-21 09:43:58 -0600

    the map works and it now gives you an alert of your coordinates!

--- src/main/resources/static/map/mapScript.js
+++ src/main/resources/static/map/mapScript.js
@@ -5,21 +5,24 @@
         });
          google.charts.setOnLoadCallback(drawChart);
       function drawChart() {
+
       let data = google.visualization.arrayToDataTable([
-                ['Lat', 'Long', 'Name'],
-                [38.6270,  -90.1994, 'St. Louis'],
-                [38.65214508187943, -90.2595139, 'LaunchCode'],
-              ]);
+                     ['Lat', 'Long', 'Name'],
+                     [38.6270,  -90.1994, 'St. Louis'],
+                     [38.65214508187943, -90.2595139, 'LaunchCode'],
+                   ]);
 
- if(navigator.geolocation){
-      navigator.geolocation.getCurrentPosition(function (position){
-      initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
-      map.setCenter(initialLocation)})
-      }
+      if(navigator.geolocation){
+           navigator.geolocation.getCurrentPosition(function (position){
+           initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+           alert("You are at: " + initialLocation);
+           })
+           }
 
-        let map = new google.visualization.Map(document.getElementById('map_div'));
-        map.draw(data, {
-          showTooltip: true,
-          showInfoWindow: true  });
+             let map = new google.visualization.Map(document.getElementById('map_div'));
+             map.draw(data, {
+               showTooltip: true,
+               showInfoWindow: true
+             });
 
-                                    }
+           }
