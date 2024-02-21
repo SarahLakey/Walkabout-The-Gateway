d64568a74ad1725095c4247318da289847861bfd
jojotier <122423763+jojotier@users.noreply.github.com>
2024-02-21 09:24:53 -0600

    it works now!

--- src/main/resources/static/map/mapScript.js
+++ src/main/resources/static/map/mapScript.js
@@ -1,27 +1,25 @@
 
+        google.charts.load("current", {
+          "packages":["map"],
+          "mapsApiKey": "AIzaSyDgfwP9_Iur5vGE5suxMVNq3qwiONfMq2I"
+        });
+         google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+      let data = google.visualization.arrayToDataTable([
+                ['Lat', 'Long', 'Name'],
+                [38.6270,  -90.1994, 'St. Louis'],
+                [38.65214508187943, -90.2595139, 'LaunchCode'],
+              ]);
 
-  /*  function initMap(){
-    map=new google.maps.Map(document.getElementId('map'), {
-    center: {lat: 38.6270, lng:-90.1994}
-    zoom: 4
-    });*/
-
-    let map;
-
-    async function initMap() {
-      const { Map } = await google.maps.importLibrary("maps");
-
-      map = new Map(document.getElementById("map"), {
-        center: { lat: 38.627, lng: -90.199 },
-        zoom: 8,
-      });
-    }
-
-    initMap();
-
-         if(navigator.geolocation){
-              navigator.geolocation.getCurrentPosition(function (position){
-              initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
-              map.setCenter(initialLocation)})
-              };
+ if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(function (position){
+      initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
+      map.setCenter(initialLocation)})
       }
+
+        let map = new google.visualization.Map(document.getElementById('map_div'));
+        map.draw(data, {
+          showTooltip: true,
+          showInfoWindow: true  });
+
+                                    }
