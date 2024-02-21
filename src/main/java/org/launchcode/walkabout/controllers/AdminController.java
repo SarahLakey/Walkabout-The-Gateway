3820edc8708d9d504358c8fb92ba034d2e597da6
sammcmurtrey <mcmurtreysamantha@gmail.com>
2024-02-20 18:14:08 -0600

    fixed report button

--- src/main/java/org/launchcode/walkabout/controllers/AdminController.java
+++ src/main/java/org/launchcode/walkabout/controllers/AdminController.java
@@ -1,78 +1,38 @@
 package org.launchcode.walkabout.controllers;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
-import org.launchcode.walkabout.data.UserRepository;
 import org.launchcode.walkabout.models.ReportButton;
 import org.launchcode.walkabout.models.SubmitFact;
-import org.launchcode.walkabout.models.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Controller
 public class AdminController {
 
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private AuthenticationController authenticationController;
-
-    private User getCurrentUser(HttpServletRequest request) {
-        HttpSession session = request.getSession();
-        return authenticationController.getUserFromSession(session);
-    }
-
     @GetMapping("/admin")
-    public String submitFactForm(HttpServletRequest request, HttpSession session, Model model) {
-        User user = getCurrentUser(request);
-
-        model.addAttribute("user", userRepository.findById(user.getId()));
-        model.addAttribute("loggedIn", session.getAttribute("user") != null);
+    public String submitFactForm(Model model) {
         model.addAttribute("submitFact", new SubmitFact());
-
-
-
-
-        return "admin/index";
-
+        return "submitFact";
     }
 
     @PostMapping("/admin")
-    public String processSubmitFact(HttpServletRequest request, Model model, @ModelAttribute SubmitFact submitFact) {
-
-        model.addAttribute("nameInput", submitFact.getNameInput());
-        model.addAttribute("locationInput", submitFact.getLocationInput());
-        model.addAttribute("factInput", submitFact.getFactInput());
+    public String submitFactForm(@ModelAttribute SubmitFact submitFact, Model model) {
         model.addAttribute("submitFact", submitFact);
-
-        return "admin/index";
+        return "result";
     }
 
-
-    @GetMapping("/report")
-    public String submitReportForm(HttpServletRequest request, HttpSession session, Model model){
-        User user = getCurrentUser(request);
-        model.addAttribute("user", userRepository.findById(user.getId()));
-        model.addAttribute("loggedIn", session.getAttribute("user") != null);
+    @GetMapping("/adminTwo")
+    public String submitReportForm(Model model){
         model.addAttribute("reportButton", new ReportButton());
-        return "admin/reportbutton";
+        return "submitReport";
     }
 
-    @PostMapping("/report")
-    public String submitReportForm(HttpServletRequest request, Model model, @ModelAttribute ReportButton reportButton) {
-        model.addAttribute("name", reportButton.getName());
-        model.addAttribute("report", reportButton.getReport());
+    @PostMapping("/adminTwo")
+    public String submitReportForm(@ModelAttribute ReportButton reportButton, Model model) {
         model.addAttribute("reportButton", reportButton);
-        return "admin/reportbutton";
+        return "result";
     }
 
 }
